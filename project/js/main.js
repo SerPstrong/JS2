0fc6dd8115288e15e20c9400cf29ebdb2cc8e0a1
Sergey Plotnikov <37042872+SerPstrong@users.noreply.github.com>
2020-03-13 16:21:53 +0500

    Стилизация, убраны запятые

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,41 @@
+// const products = [
+//     {id: 1, title: 'Notebook', price: 20000},
+//     {id: 2, title: 'Mouse', price: 1500},
+//     {id: 3, title: 'Keyboard', price: 5000},
+//     {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (title, price) => {
+//     return `<div class="product-item">
+//                 <h3>${title}</h3>
+//                 <p>${price}</p>
+//                 <button class="buy-btn">Добавить в корзину</button>
+//             </div>`;
+// };
+//
+// const renderProducts = list => {
+//     document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price));
+// };
+//
+// renderProducts(products);
+
 const products = [
     {id: 1, title: 'Notebook', price: 20000},
     {id: 2, title: 'Mouse', price: 1500},
     {id: 3, title: 'Keyboard', price: 5000},
     {id: 4, title: 'Gamepad', price: 4500},
 ];
+
+document.querySelector('.app').innerHTML = `<header>
+    <div class="container">
+        <button class="btn-cart" type="button">Корзина</button>
+    </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="products"></div>
+        </div>
+    </main>`;
 
 const renderProduct = (title, price) => {
     return `<div class="product-item">
@@ -14,7 +46,7 @@ const renderProduct = (title, price) => {
 };
 
 const renderProducts = list => {
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price));
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
 };
 
 renderProducts(products);
